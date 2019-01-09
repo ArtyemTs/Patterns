de49695748ded4c288846b3c35ebb27be1925895
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-01-09 18:21:31 +0300

    EventListener

--- src/DemoObserver.java
+++ src/DemoObserver.java
@@ -1,6 +1,6 @@
 import thread_safe.Singleton;
 
-public class DemoSingletonSynch {
+public class DemoObserver {
     public static void main(String[] args) {
         System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
                 "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +

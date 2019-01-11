93cf1516c489f72f294eaaa9eeed1be61d64fed1
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-01-11 15:12:36 +0300

    Builder (complete)

--- src/builders/Builder.java
+++ src/builders/Builder.java
@@ -1,10 +1,16 @@
 package builders;
 
+import cars.Type;
+import componets.Engine;
+import componets.GPSNavigator;
+import componets.Transmission;
+import componets.TripComputer;
+
 public interface Builder {
-    void setType();
+    void setType(Type type);
     void setSeats(int seats);
-    void setEngine();
-    void setTransmission();
-    void setTripConputer();
-    void setGPSNavigator();
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripConputer(TripComputer tripConputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
 }

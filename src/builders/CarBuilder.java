48e971afb1e6a79a036f2dc1f639e7bb7643e8f8
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-01-11 15:22:23 +0300

    CarBuilder

---
+++ src/builders/CarBuilder.java
@@ -0,0 +1,52 @@
+package builders;
+
+import cars.Car;
+import cars.Type;
+import componets.Engine;
+import componets.GPSNavigator;
+import componets.Transmission;
+import componets.TripComputer;
+
+public class CarBuilder implements Builder {
+
+    private Type type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    @Override
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripConputer(TripComputer tripConputer) {
+        this.tripComputer = tripConputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Car getResult(){
+        return new Car(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}

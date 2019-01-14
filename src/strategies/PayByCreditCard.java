9defd004d68a3724b7dbae95182905337ff3b498
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-01-14 16:36:13 +0300

    CreditCard

---
+++ src/strategies/PayByCreditCard.java
@@ -0,0 +1,10 @@
+package strategies;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PayByCreditCard implements PayStrategy {
+
+    private final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private CredetCard card;
+}
